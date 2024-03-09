ae2bf808171bba7e635b4c7cd2ab53820e233e12
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-03-08 23:07:12 -0300

    chore: install and config shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-export function App() {
+import { Button } from "@/components/ui/button"
 
+import './global.css'
+
+export function App() {
   return (
-    <h1>Pizza shop</h1>
+    <div className="flex justify-center items-center h-screen">
+      <Button>Enviar</Button>
+    </div>
   )
 }
