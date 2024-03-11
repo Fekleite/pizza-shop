f4230feaaf5947a347b477748a4643624e4f3d91
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-03-11 20:57:47 -0300

    feat: create theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,18 @@ import { RouterProvider } from 'react-router-dom'
 
 import { Toaster } from '@/components/ui/sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster />
+      <ThemeProvider storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster />
 
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
