e7caae0ae761f6d55635d393b5fc378bd2d93f51
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-03-17 12:23:33 -0300

    feat: add filters action

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -19,14 +19,24 @@ import { OrderTableRow } from './order-table-row'
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams()
 
+  const orderId = searchParams.get('orderId')
+  const customerName = searchParams.get('customerName')
+  const status = searchParams.get('status')
+
   const pageIndex = z.coerce
     .number()
     .transform((page) => page - 1)
     .parse(searchParams.get('page') ?? '1')
 
   const { data: result } = useQuery({
-    queryFn: () => getOrders({ pageIndex }),
-    queryKey: ['orders', pageIndex],
+    queryFn: () =>
+      getOrders({
+        pageIndex,
+        orderId,
+        customerName,
+        status: status === 'all' ? null : status,
+      }),
+    queryKey: ['orders', pageIndex, orderId, customerName, status],
   })
 
   function handlePaginate(pageIndex: number) {
