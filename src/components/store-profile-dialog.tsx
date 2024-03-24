7089882a5a94ba82e8478fedc78d68d0dc40bd6f
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-03-24 11:27:43 -0300

    fix: change description label text

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -114,7 +114,7 @@ export function StoreProfileDialog() {
 
           <div className="grid grid-cols-4 items-center gap-4">
             <Label className="text-right" htmlFor="description">
-              Nome
+              Descrição
             </Label>
             <Textarea
               className="col-span-3"
